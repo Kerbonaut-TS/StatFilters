1a88f7477f11812098efd753caccc0c69733eeb1
kerbonaut-TS <valentinuzzi.r@outlook.com>
2023-10-29 17:31:25 +0000

    unit test

--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,23 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-		final String path= "C:\\path\\to\\image\\input";
+		final String filepath= "C:\\path\\to\\image\\input";
 		
 
-		StatFilter filter= new StatFilter();
-		
-		filter.setSource(path);
-		filter.createTiles("5x5");
-	
-			
-		BufferedImage  img;
+		StatFilter f1= new StatFilter();
+		f1.setSource(filepath);
 
-		img = filter.showTiles();
-		
-		filter.applyOperation("mean");
-		
-		System.out.println("end test");
-		
-		filter.savefile("C:\\path\\to\\image\\output", "png");
+		String [] operations = new String[]{"mean", "std.dev", "sobel", "sqrt", "entropy", "red", "green", "blue", ""};
+
+		for (String operation: operations) {
+		    System.out.println("Applying filer: "+operation);
+		    f1.createTiles("5x5");
+		    f1.applyOperation(operation);
+		    f1.saveImage("test//"+operation+".jpg", "jpg");
+		    f1.reset();
+
+		} 
+
 
 
 	}//end main
